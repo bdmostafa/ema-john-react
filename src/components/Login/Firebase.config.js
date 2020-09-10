3eb41480a299e50a00641c4d4174341c4efdfc93
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-10 18:19:05 +0600

    login management and code refactoring done

---
+++ src/components/Login/Firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzSub4arpBxmv63zkFZ-W46S-lqz2AXpw",
+    authDomain: "emma-john-react.firebaseapp.com",
+    databaseURL: "https://emma-john-react.firebaseio.com",
+    projectId: "emma-john-react",
+    storageBucket: "emma-john-react.appspot.com",
+    messagingSenderId: "201982634557",
+    appId: "1:201982634557:web:f7182e26dec4fb5ca65854"
+  };
+
+export default firebaseConfig;
